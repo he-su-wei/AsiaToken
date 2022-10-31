37d066ac15609c3e8429a9558a12c2e1d6c6f270
He-Shu-Wei <109021372@live.asia.edu.tw>
2022-10-31 19:14:55 +0800

    change getval

--- LineBot/AsiaToken/connection.py
+++ LineBot/AsiaToken/connection.py
@@ -30,8 +30,8 @@ def setImage(userId, imgUrl):
         print(result) #success
     return result
 
-
-def getData(userId): #address&url
+# 取得帳戶地址及圖片位置
+def getval(userId): #address&url
     url = 'http://120.108.111.229:8080/getVal'
     values = {'userID': userId}
 
@@ -41,7 +41,52 @@ def getData(userId): #address&url
     with urllib.request.urlopen(req) as response:
         the_page = response.read().decode('utf-8')
         result = the_page.split(",")
-        print(result[0])
-        print(result[1])
+        print(result[0]) # address 
+        print(result[1]) # url
     return result
     #getVal
+
+# 取得帳戶餘額
+def accountBalance(userId): #address&url
+    url = 'http://120.108.111.229:8080/accountBalance'
+    values = {'userID': userId}
+
+    data = urllib.parse.urlencode(values)
+    data = data.encode('ascii')
+    req = urllib.request.Request(url, data)
+    with urllib.request.urlopen(req) as response:
+        the_page = response.read()
+        result = the_page.decode('utf-8')
+        print(result) # int
+    return result
+
+# 取得交易紀錄
+def getAccountData(userId): #address&url
+    url = 'http://120.108.111.229:8080/accountBalance'
+    values = {'userID': userId}
+
+    data = urllib.parse.urlencode(values)
+    data = data.encode('ascii')
+    req = urllib.request.Request(url, data)
+    with urllib.request.urlopen(req) as response:
+        the_page = response.read()
+        result = the_page.decode('utf-8')
+        print(result[0]) # from:'user'
+        print(result[1]) # to : 'userAddress'
+        print(result[2]) # value : "count" 交易數量
+        print(result[3]) # time : 'timestamp'
+    return result
+
+# 交易
+def transferAUT(userId,value): #address&url
+    url = 'http://120.108.111.229:8080/transferAUT'
+    values = {'userID': userId}
+
+    data = urllib.parse.urlencode(values)
+    data = data.encode('ascii')
+    req = urllib.request.Request(url, data)
+    with urllib.request.urlopen(req) as response:
+        the_page = response.read()
+        result = the_page.decode('utf-8')
+        print(result) # true
+    return result
